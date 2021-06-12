37972f5dccfda279d4aad971762e63fc9d4b3bee
Aditya Yusril Fikri <38427057+yusrilAditya98@users.noreply.github.com>
2021-06-12 10:06:48 +0700

    php 8
    update terbaru

--- application/helpers/excel_reader_helper.php
+++ application/helpers/excel_reader_helper.php
@@ -606,7 +606,7 @@ class Spreadsheet_Excel_Reader
 		}
 		return null;
 	}
-	function fontProperty($row, $col, $sheet = 0, $prop)
+	function fontProperty($row, $col, $sheet = 0, $prop = "")
 	{
 		$font = $this->fontRecord($row, $col, $sheet);
 		if ($font != null) {
